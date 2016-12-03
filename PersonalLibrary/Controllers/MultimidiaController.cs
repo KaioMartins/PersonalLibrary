483ec2c461068bf1fa3d1263f402e306e28d24bc
KaioMartins <kaio.ccmartins@gmail.com>
2016-12-03 18:02:00 -0200

    Alterações para outra apresentação Adicão de Vídeos e podcasts

---
+++ PersonalLibrary/Controllers/MultimidiaController.cs
@@ -0,0 +1,30 @@
+﻿using PersonalLibrary.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalLibrary.Controllers
+{
+    public class MultimidiaController : Controller
+    {
+        // GET: Multimidia
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AuthFilter]
+        public ActionResult Video()
+        {
+            return View();
+        }
+
+        [AuthFilter]
+        public ActionResult Audio()
+        {
+            return View();
+        }
+    }
+}
